83e4e78f627af6db744ad31a45b528f7f6f63f0f
Sviatoslav Samoilenko <sviatoslavs@wizardsdev.com>
2021-06-29 22:49:59 +0300

    Add project files.

---
+++ EFCoreExample.BAL/EFCoreContext.cs
@@ -0,0 +1,25 @@
+﻿using EFCoreExample.BAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreExample.BAL
+{
+    public class EfCoreContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public EfCoreContext(DbContextOptions<EfCoreContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ EFCoreExample/BooksController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreExample.BAL.Models;
+
+namespace EFCoreExample
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private BooksService _booksService;
+        public BooksController(BooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetAllBooks()
+        {
+            return await _booksService.GetAllBooks();
+        }
+
+        [HttpPost]
+        public async Task AddBook(Book book)
+        {
+            book.Id = Guid.NewGuid();
+            await _booksService.AddBook(book);
+        }
+    }
+}
